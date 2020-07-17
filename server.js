09dde2c8c3df658d114904103a6b53e5fa2a57b8
Isaac Aguiar <isaacaguiar_@hotmail.com>
2020-07-17 19:34:22 -0300

    server cleaning

--- server.js
+++ server.js
@@ -1,18 +1,14 @@
 //Importação das funções
 const express = require('express')
 const nunjucks = require('nunjucks')
-const receitas = require('./data') // Importa arquivo externo
+const routes = require('./routes')
 
 //Chamada da função
 const server = express()
 
-//Abertura de porta
-server.listen(5000, function(){
-    console.log('Server Ok!')
-})
-
-//Importação de arquivos estáticos
+//Middlewares
 server.use(express.static('public'))
+server.use(routes)
 
 //Configuração da template Engine
 server.set('view engine', 'njk')
@@ -23,28 +19,7 @@ nunjucks.configure('src', {
     noCache: true //Não armazena os sites em cache
 })
 
-
-//Criação de rotas
-server.get('/', function(req, res){
-    res.render('home',  { items: receitas })
-})
-
-server.get('/about', function(req, res){
-    res.render('about')
-})
-
-server.get('/recipes', function(req,res){
-    res.render('recipes', { items: receitas })
-})
-
-// server.get('/recipes/:id', function(req, res) {
-//     const id = req.params.id
-//     return res.render('recipe', { items: receitas })
-// })
-
-server.get('/recipes/:index', function(req, res){
-    const recipes = receitas
-    const recipeIndex = req.params.index
-    
-    res.render('description', {item: recipes[recipeIndex]})
+//Abertura de porta
+server.listen(5000, function(){
+    console.log('Server Ok!')
 })

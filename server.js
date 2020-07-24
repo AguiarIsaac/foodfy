1d8ec8db316393dbc73e3320103bd14127282c5d
Isaac Aguiar <isaacaguiar_@hotmail.com>
2020-07-23 21:38:22 -0300

    added Midleware

--- server.js
+++ server.js
@@ -7,8 +7,10 @@ const routes = require('./routes')
 const server = express()
 
 //Middlewares
+server.use(express.urlencoded({ extended: true }))
 server.use(express.static('public'))
 server.use(routes)
+
 
 //Configuração da template Engine
 server.set('view engine', 'njk')

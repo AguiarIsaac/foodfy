7382134e28d40fc35726dde39500c91a3ea4725f
Isaac Aguiar <isaacaguiar_@hotmail.com>
2020-07-26 19:19:06 -0300

    alters

--- routes.js
+++ routes.js
@@ -6,13 +6,14 @@ const admin = require('./controllers/admin_recipes')
 routes.get('/', recipes.index )
 routes.get('/about', recipes.about )
 routes.get('/recipes', recipes.recipes )
-routes.get('/recipes/:index', recipes.description )
+routes.get('/recipes/:id', recipes.description )
 
 routes.get('/admin/recipes', admin.index )
 routes.get('/admin', admin.home )
 routes.get('/admin/recipes/create', admin.create )
 routes.post('/admin/recipes/create', admin.post )
 routes.get('/admin/recipes/:id', admin.show )
-routes.get('/admin/recipes/edit', admin.edit )
+routes.get('/admin/recipes/:id/edit', admin.edit )
+routes.put('/admin/recipes', admin.put )
 
 module.exports = routes
